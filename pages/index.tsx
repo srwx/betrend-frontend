5feb2638cd3b80330e0ac8b71f616232bcb6494d
Sorawong L <Sorawong2543@gmail.com>
2022-07-23 01:05:04 +0700

    update Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
 import { Header } from "component/Header/Header"
 import type { NextPage } from "next"
 import { HomeHero } from "pages/Home/HomeHero"
+import { HomeContainer } from "styles/index"
 
 const Home: NextPage = () => {
   return (
     <>
-      <Header />
-      <HomeHero />
+      <HomeContainer>
+        <HomeHero />
+      </HomeContainer>
     </>
   )
 }
