878ffd9b5c4de56c63077b7ec4ddde48e08ebaf3
Sorawong L <Sorawong2543@gmail.com>
2022-07-23 01:04:14 +0700

    added `<Header />` for all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import "styles/global.css"
 import type { AppProps } from "next/app"
+import { Header } from "component/Header/Header"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
