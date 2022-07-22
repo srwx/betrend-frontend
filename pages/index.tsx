2f17e93f743672714324ac86d58715a6c1c4f79c
Sorawong L <Sorawong2543@gmail.com>
2022-07-22 23:08:21 +0700

    initial `<Header />`

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import { Header } from "component/Header/Header"
 import type { NextPage } from "next"
 import { HomeHero } from "pages/Home/HomeHero"
 
 const Home: NextPage = () => {
   return (
     <>
+      <Header />
       <HomeHero />
     </>
   )
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,15 @@
 import React from "react"
+import styled from "styled-components"
+
+const Container = styled.div`
+  background: linear-gradient(
+    81.88deg,
+    #352489 -14.13%,
+    #0b0013 53.72%,
+    #7a2d7a 124.1%
+  );
+`
 
 export const Header = () => {
-  return <div>Header</div>
+  return <Container>Header</Container>
 }
