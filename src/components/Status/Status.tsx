a3fa3532aaaf29445dbb92f06941f94876bc7de8
Sorawong L <Sorawong2543@gmail.com>
2022-07-24 00:07:42 +0700

    update all status style

--- src/components/Status/Status.tsx
+++ src/components/Status/Status.tsx
@@ -2,74 +2,117 @@ import classNames from "classnames"
 import React from "react"
 import styled from "styled-components"
 
+const VoteContainer = styled.div`
+  width: 4.5rem;
+  height: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  z-index: 0;
+
+  ::before {
+    content: "";
+    position: absolute;
+    z-index: -1;
+    inset: 0;
+    padding: 1.5px; /* border size */
+    border-radius: 10px;
+    background: linear-gradient(to right, #5834f8, #0faeff);
+    -webkit-mask: linear-gradient(#fff 0 0) content-box,
+      linear-gradient(#fff 0 0);
+    mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
+    -webkit-mask-composite: xor;
+    mask-composite: exclude;
+  }
+`
+
 export const Vote = () => {
   return (
-    <div
-      className={classNames(
-        "w-[4.5rem] h-[2rem]", // base
-        "rounded-lg flex justify-center items-center",
-        "bg-gradient-to-r from-[#5834F8] to-[#0FAEFF]"
-      )}
-    >
-      <div className="rounded-lg w-[95%] h-[90%] bg-[#0B0013] flex justify-center items-center">
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#5834F8] to-[#0FAEFF] font-semibold">
-          Vote
-        </span>
-      </div>
-    </div>
+    <VoteContainer>
+      <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#5834F8] to-[#0FAEFF] font-semibold">
+        Vote
+      </span>
+    </VoteContainer>
   )
 }
 
 export const Bet = () => {
   return (
-    <div
-      className={classNames(
-        "w-[4.5rem] h-[2rem]", // base
-        "rounded-lg flex justify-center items-center",
-        "bg-gradient-to-r from-[#5834F8] to-[#0FAEFF]"
-      )}
-    >
-      <div className="rounded-lg w-[95%] h-[90%] bg-[#0B0013] flex justify-center items-center">
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#5834F8] to-[#0FAEFF] font-semibold">
-          Bet
-        </span>
-      </div>
-    </div>
+    <VoteContainer>
+      <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#5834F8] to-[#0FAEFF] font-semibold">
+        Bet
+      </span>
+    </VoteContainer>
   )
 }
+
+const ActiveContainer = styled.div`
+  width: 5.5rem;
+  height: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  z-index: 0;
+
+  ::before {
+    content: "";
+    position: absolute;
+    z-index: -1;
+    inset: 0;
+    padding: 1.5px; /* border size */
+    border-radius: 10px;
+    background: linear-gradient(to right, #00a6c7, #20fda5);
+    -webkit-mask: linear-gradient(#fff 0 0) content-box,
+      linear-gradient(#fff 0 0);
+    mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
+    -webkit-mask-composite: xor;
+    mask-composite: exclude;
+  }
+`
 
 export const Active = () => {
   return (
-    <div
-      className={classNames(
-        "w-[5.5rem] h-[2rem]", // base
-        "rounded-lg flex justify-center items-center",
-        "bg-gradient-to-r from-[#00A6C7] to-[#20FDA5]"
-      )}
-    >
-      <div className="rounded-lg w-[95%] h-[90%] bg-[#0B0013] flex justify-center items-center">
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#00A6C7] to-[#20FDA5] font-semibold">
-          Active
-        </span>
-      </div>
-    </div>
+    <ActiveContainer>
+      <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#00A6C7] to-[#20FDA5] font-semibold">
+        Active
+      </span>
+    </ActiveContainer>
   )
 }
 
+const ClosedContainer = styled.div`
+  width: 5.5rem;
+  height: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  z-index: 0;
+
+  ::before {
+    content: "";
+    position: absolute;
+    z-index: -1;
+    inset: 0;
+    padding: 1.5px; /* border size */
+    border-radius: 10px;
+    background: linear-gradient(to right, #d4145a, #fbb030);
+    -webkit-mask: linear-gradient(#fff 0 0) content-box,
+      linear-gradient(#fff 0 0);
+    mask: linear-gradient(#fff 0 0) content-box, linear-gradient(#fff 0 0);
+    -webkit-mask-composite: xor;
+    mask-composite: exclude;
+  }
+`
+
 export const Closed = () => {
   return (
-    <div
-      className={classNames(
-        "w-[5.5rem] h-[2rem]", // base
-        "rounded-lg flex justify-center items-center",
-        "bg-gradient-to-r from-[#D4145A] to-[#FBB030]"
-      )}
-    >
-      <div className="rounded-lg w-[95%] h-[90%] bg-[#0B0013] flex justify-center items-center">
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#D4145A] to-[#FBB030] font-semibold">
-          Closed
-        </span>
-      </div>
-    </div>
+    <ClosedContainer>
+      <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#D4145A] to-[#FBB030] font-semibold">
+        Closed
+      </span>
+    </ClosedContainer>
   )
 }
