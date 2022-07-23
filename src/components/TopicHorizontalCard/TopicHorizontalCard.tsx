a162406d3c0d03b74f8d9aa77198883de58c9d69
Sorawong L <Sorawong2543@gmail.com>
2022-07-23 20:44:46 +0700

    list of topics in sort topic section

--- src/components/TopicHorizontalCard/TopicHorizontalCard.tsx
+++ src/components/TopicHorizontalCard/TopicHorizontalCard.tsx
@@ -1,5 +1,47 @@
+import { Active, Bet, Closed, Vote } from "component/Status/Status"
 import React from "react"
+import styled from "styled-components"
 
-export const TopicHorizontalCard = () => {
-  return <div className="w-full h-[5rem] bg-red-400">TopicHorizontalCard</div>
+const Container = styled.div`
+  width: 100%;
+  height: 5rem;
+  background: rgba(137, 137, 137, 0.2);
+  box-shadow: 0px 3.34566px 35.1295px rgba(0, 0, 0, 0.15);
+  border-radius: 10px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  color: white;
+`
+
+interface TopicHorizontalCardProps {
+  place: number
+  topic: string
+  prize: number
+  isActive: boolean
+  isVote: boolean
+}
+
+export const TopicHorizontalCard = ({
+  place,
+  topic,
+  prize,
+  isActive,
+  isVote,
+}: TopicHorizontalCardProps) => {
+  return (
+    <Container>
+      <div className="flex-[0.2]">
+        <div className="w-[2.4rem] h-[2.4rem] ml-8 rounded-full border-white border-solid border-[1px] flex justify-center items-center">
+          {place}
+        </div>
+      </div>
+      <span className="flex-[0.4]">{topic}</span>
+      <span className="flex-[0.25]">{prize}</span>
+      <div className="flex-[0.2] flex gap-2">
+        {isActive ? <Active /> : <Closed />}
+        {isVote ? <Vote /> : <Bet />}
+      </div>
+    </Container>
+  )
 }
--- src/pages/Home/SortTopics.tsx
+++ src/pages/Home/SortTopics.tsx
@@ -1,12 +1,30 @@
 import { TopicHorizontalCard } from "component/TopicHorizontalCard/TopicHorizontalCard"
 import React from "react"
 import { HomeContainer } from "styles/index"
+import allTopics from "./utils/getAllTopics.json"
 
 export const SortTopics = () => {
   return (
     <HomeContainer>
       <div className="px-[2.5rem]">
-        <TopicHorizontalCard />
+        <ul className="flex list-none text-2xl text-[#B5BDC1] mb-12">
+          <li className="flex-[0.2]">Place</li>
+          <li className="flex-[0.4]">Topics</li>
+          <li className="flex-[0.25]">Prize</li>
+          <li className="flex-[0.2]">Status</li>
+        </ul>
+        <div className="space-y-6">
+          {allTopics.map((topic, i) => (
+            <TopicHorizontalCard
+              key={i}
+              place={i + 1}
+              topic={topic.title}
+              prize={topic.prize}
+              isActive={topic.isActive}
+              isVote={topic.isVote}
+            />
+          ))}
+        </div>
       </div>
     </HomeContainer>
   )
