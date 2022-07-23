571a0382aa50d102504268b9588441cdd0e0adc9
Sorawong L <Sorawong2543@gmail.com>
2022-07-23 21:11:59 +0700

    init `<Footer />`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import "styles/global.css"
 import type { AppProps } from "next/app"
 import { Header } from "component/Header/Header"
+import { Footer } from "component/Footer/Footer"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import styled from "styled-components"
+
+const Container = styled.div`
+  width: 100%;
+  height: 7rem;
+  background: rgba(217, 217, 217, 0.2);
+  padding: 0 60px;
+  display: flex;
+  align-items: center;
+  border-top: 0.001rem solid gray;
+`
+
+export const Footer = () => {
+  return <Container>Footer</Container>
+}
