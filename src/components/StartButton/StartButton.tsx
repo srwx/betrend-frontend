7d91bd830756a670530627e12144a2c2fb26c45c
Sorawong L <Sorawong2543@gmail.com>
2022-07-23 09:35:49 +0700

    added `<StartButton />`

---
+++ src/components/StartButton/StartButton.tsx
@@ -0,0 +1,36 @@
+import classNames from "classnames"
+import Image from "next/image"
+import React from "react"
+import styled from "styled-components"
+
+const Container = styled.div`
+  width: 12rem;
+  height: 3rem;
+  border-radius: 0.5rem;
+  background-color: #8a3bf0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 2.5rem;
+  cursor: pointer;
+
+  :hover {
+    background: linear-gradient(
+      81.88deg,
+      #8a3bf0 34.96%,
+      #6274f3 71.34%,
+      #3ea7f6 98.63%,
+      #00fffb 124.1%
+    );
+  }
+`
+export const StartButton = () => {
+  return (
+    <Container>
+      <span className="text-white font-semibold text-lg tracking-wider">
+        Get Started
+      </span>
+      <Image src="/icons/arrow.png" alt="arrow" width="15" height="12" />
+    </Container>
+  )
+}
