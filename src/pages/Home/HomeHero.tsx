6fc6cc566ee48fe97b5646e50faffa2bed30d83a
Sorawong L <Sorawong2543@gmail.com>
2022-07-24 15:54:15 +0700

    hero animation

--- src/pages/Home/HomeHero.tsx
+++ src/pages/Home/HomeHero.tsx
@@ -3,8 +3,61 @@ import { LearnMoreButton } from "component/Button/LearnmoreButton/LearnMoreButto
 import { StartButton } from "component/Button/StartButton/StartButton"
 import Image from "next/image"
 import React, { forwardRef, MutableRefObject } from "react"
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
 import { HomeContainer } from "styles/index"
+
+const fadeOut = keyframes`
+  0% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0;
+    transform: scale(85%);
+  }
+`
+
+const scrollUp = keyframes`
+  100% {
+    transform: translateY(-6px);
+  }
+`
+
+const scrollDown = keyframes`
+  100%{
+    transform: translateY(6px);
+  }
+`
+
+const RotateWrapper1 = styled.div`
+  position: absolute;
+  top: 33.2%;
+  left: 43.1%;
+  /* transform-style: preserve-3d;
+  transform: perspective(500px) rotateX(45deg); */
+  animation: ${fadeOut} 4s linear infinite alternate;
+`
+
+const RotateWrapper2 = styled.div`
+  position: absolute;
+  top: 24.6%;
+  left: 19%;
+  animation: ${scrollUp} 3s linear infinite alternate;
+`
+
+const RotateWrapper3 = styled.div`
+  position: absolute;
+  top: 16.9%;
+  left: 26.5%;
+  animation: ${scrollDown} 3.5s linear infinite alternate;
+`
+
+const RotateWrapper4 = styled.div`
+  position: absolute;
+  top: 12.9%;
+  left: 35.5%;
+  animation: ${scrollUp} 4s linear infinite alternate;
+`
 
 export const HomeHero = forwardRef<HTMLDivElement>((_, ref) => {
   const scrollToAllTopicsSection = () =>
@@ -15,11 +68,9 @@ export const HomeHero = forwardRef<HTMLDivElement>((_, ref) => {
   return (
     <HomeContainer>
       <div
-        className={classNames(
-          "px-[2.5rem] py-[4rem] flex items-center gap-x-20 w-full"
-        )}
+        className={classNames("px-[2.5rem] py-[4rem] flex items-center w-full")}
       >
-        <div className="flex-[0.55] space-y-10">
+        <div className="flex-[0.5] space-y-10">
           <article className={classNames("text-white space-y-6")}>
             <h1 className={classNames("text-4xl font-semibold")}>
               The Easiest Way to
@@ -42,13 +93,93 @@ export const HomeHero = forwardRef<HTMLDivElement>((_, ref) => {
             <LearnMoreButton />
           </div>
         </div>
-        <div className="flex-[0.45]">
+        <div className="flex-[0.5] relative">
           <Image
-            src="/gray-rectangle.png"
+            src="/images/hero/bg.png"
             alt="temp"
-            width="590"
-            height="480"
+            width="3267"
+            height="2630"
           />
+          <div className="absolute top-[53%] left-[8%] animate-[floatingUp_3.2s_linear_infinite_alternate]">
+            <Image
+              src="/images/hero/diamond.png"
+              alt="eth"
+              width="21"
+              height="42"
+            />
+          </div>
+          <div className="absolute top-[58%] left-[19.2%] animate-[floatingDown_3s_linear_infinite_alternate]">
+            <Image
+              src="/images/hero/diamond.png"
+              alt="eth"
+              width="21"
+              height="42"
+            />
+          </div>
+          <div className="absolute top-[69%] left-[30.3%] animate-[floatingUp_3.2s_linear_infinite_alternate]">
+            <Image
+              src="/images/hero/diamond.png"
+              alt="eth"
+              width="21"
+              height="42"
+            />
+          </div>
+          <div className="absolute top-[57.3%] right-[19.5%] animate-[floatingUp_3.2s_linear_infinite_alternate]">
+            <Image
+              src="/images/hero/chart1.png"
+              alt="eth"
+              width="38"
+              height="58"
+            />
+          </div>
+          <div className="absolute top-[57%] right-[15.8%] animate-[floatingUp_3.2s_linear_infinite_alternate]">
+            <Image
+              src="/images/hero/chart2.png"
+              alt="eth"
+              width="36"
+              height="50"
+            />
+          </div>
+          <div className="absolute top-[55%] right-[12.4%] animate-[floatingUp2_3.2s_linear_infinite_alternate]">
+            <Image
+              src="/images/hero/chart2.png"
+              alt="eth"
+              width="36"
+              height="50"
+            />
+          </div>
+          <RotateWrapper1>
+            <Image
+              src="/images/hero/circle1.png"
+              alt="eth"
+              width="77"
+              height="57"
+            />
+          </RotateWrapper1>
+          <RotateWrapper2>
+            <Image
+              src="/images/hero/circle2.png"
+              alt="eth"
+              width="27"
+              height="47"
+            />
+          </RotateWrapper2>
+          <RotateWrapper3>
+            <Image
+              src="/images/hero/circle3.png"
+              alt="eth"
+              width="37"
+              height="61"
+            />
+          </RotateWrapper3>
+          <RotateWrapper4>
+            <Image
+              src="/images/hero/circle4.png"
+              alt="eth"
+              width="27"
+              height="47"
+            />
+          </RotateWrapper4>
         </div>
       </div>
     </HomeContainer>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        floatingUp: {
+          "100%": { transform: "translateY(-0.6rem)" },
+        },
+        floatingUp2: {
+          "100%": { transform: "translateY(-0.3rem)" },
+        },
+        floatingDown: {
+          "100%": { transform: "translateY(0.5rem)" },
+        },
+      },
+    },
   },
   plugins: [],
 }
