2c38cd59b3ce2b648101852cf8c0daddff018a62
Sorawong L <Sorawong2543@gmail.com>
2022-07-24 17:57:37 +0700

    added interface for `Topic`

--- pages/topic/[address].tsx
+++ pages/topic/[address].tsx
@@ -1,11 +1,24 @@
+import { GetServerSideProps, GetServerSidePropsContext } from "next"
 import React from "react"
 import allTopics from "src/pages/Home/utils/getAllTopics.json"
 
-export default function Topic({ topic }) {
+interface TopicProps {
+  address: string
+  backgroundUrl: string
+  title: string
+  deadline: string
+  isVote: boolean
+  isActive: boolean
+  prize: number
+}
+
+export default function Topic({ topic }: { topic: TopicProps }) {
   return <div>{topic.title}</div>
 }
 
-export async function getServerSideProps(context) {
+export const getServerSideProps: GetServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
   const { address } = context.query
   const topic = allTopics.find((topic) => topic.address === address)
   return {
