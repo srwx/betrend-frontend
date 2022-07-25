1aab74dde8873378a5c02d0a72f7f3760a4a0a14
Sorawong L <Sorawong2543@gmail.com>
2022-07-26 01:14:43 +0700

    [Feature/LandingPage] Hero animation (#3)
    * hero animation
    
    * update text size
    
    * added shadow animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        floatingUp: {
+          "100%": { transform: "translateY(-0.75rem)" },
+        },
+        floatingUp2: {
+          "100%": { transform: "translateY(-0.3rem)" },
+        },
+        floatingDown: {
+          "100%": { transform: "translateY(0.9rem)" },
+        },
+        fade: {
+          "50%": { opacity: "1" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        fade: "fade 2.5s linear infinite alternate",
+      },
+    },
   },
   plugins: [],
 }
