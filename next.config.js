2db1663308db1af427aadb9dd850e31cd11da321
Nanthakarn Limkool <nanthakarn18@gmail.com>
2022-07-27 13:25:45 +0700

    update url api

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env: {
-    apiUrl: "https://c772-171-103-207-66.ap.ngrok.io",
+    apiUrl: "https://254d-171-103-207-66.ap.ngrok.io",
   },
 }
 
